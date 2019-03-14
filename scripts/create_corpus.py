0c0652b0182dbb26e4571c383278e00668042837
Juraj Pancik <jpancik@gmail.com>
2019-03-14 17:48:13 +0100

    Fixed typo.

--- scripts/create_corpus.py
+++ scripts/create_corpus.py
@@ -69,7 +69,7 @@ class CreateCorpus:
         print('Running compilecorp')
         if not self.args.debug:
             compilecorp_process = subprocess.Popen(
-                ['compilecorp', 'masters-thesis/data/compilecorp_config/dezinfo'], env=env_with_py2)
+                ['compilecorp', 'masters-thesis/files/compilecorp_config/dezinfo'], env=env_with_py2)
             compilecorp_process.wait()
 
 
